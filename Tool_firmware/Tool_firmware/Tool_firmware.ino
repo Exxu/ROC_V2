fb16e605edc036e4ba7cd8193c3c1f6320426952
Richard Andrade <ra.andrade.alfaro@gmail.com>
2026-05-29 23:58:42 -0300

    Tool comunicating via rs-486

--- Tool_firmware/Tool_firmware/Tool_firmware.ino
+++ Tool_firmware/Tool_firmware/Tool_firmware.ino
@@ -1,33 +1,45 @@
+#include <SoftwareSerial.h>
+
+#define RX_PIN 8   // D8 recibe datos
+#define TX_PIN 7   // D7 transmite datos
+#define TOOL_DOWN_PIN 4   // D7 transmite datos
+#define TOOL_UP_PIN 5   // D7 transmite datos
+
+
+SoftwareSerial rs485Serial(RX_PIN, TX_PIN);
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(4, OUTPUT);  // Configura el pin D2 como salida
+  pinMode(TOOL_DOWN_PIN, OUTPUT);  // Configura el pin D2 como salida
   pinMode(5, OUTPUT);  // Configura el pin D2 como salida
-  digitalWrite(5, HIGH); // Enciende / manda 5V al pin D2
-  digitalWrite(4, HIGH); // Enciende / manda 5V al pin D2
+  digitalWrite(TOOL_UP_PIN, HIGH); // Enciende / manda 5V al pin D2
+  digitalWrite(TOOL_DOWN_PIN, HIGH); // Enciende / manda 5V al pin D2
+
+   Serial.begin(9600);     // Serial USB hacia la PC
+  rs485Serial.begin(9600);   // Serial por software en D8/D7
+
+  Serial.println("Arduino listo");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    upTool(); // Enciende / manda 5V al pin D2
-    delay(1000);
-
-    downTool();  // Apaga / manda 0V al pin D2
-    delay(1000);
+  rs485Serial.println("Hola");
+  delay(1000);
 }
 
 void upTool(){
-  digitalWrite(4, HIGH); // Enciende / manda 5V al pin D2
+  digitalWrite(TOOL_DOWN_PIN, HIGH); // Enciende / manda 5V al pin D2
   delay(100);
-  digitalWrite(5, LOW); // Enciende / manda 5V al pin D2
+  digitalWrite(TOOL_UP_PIN, LOW); // Enciende / manda 5V al pin D2
   delay(5000);
-  digitalWrite(5, HIGH); // Enciende / manda 5V al pin D2
+  digitalWrite(TOOL_UP_PIN, HIGH); // Enciende / manda 5V al pin D2
 }
 
 void downTool(){
-  digitalWrite(5, HIGH); // Enciende / manda 5V al pin D2
+  digitalWrite(TOOL_UP_PIN, HIGH); // Enciende / manda 5V al pin D2
   delay(100);
-  digitalWrite(4, LOW); // Enciende / manda 5V al pin D2
+  digitalWrite(TOOL_DOWN_PIN, LOW); // Enciende / manda 5V al pin D2
   delay(5000);
-  digitalWrite(4, HIGH); // Enciende / manda 5V al pin D2
+  digitalWrite(TOOL_DOWN_PIN, HIGH); // Enciende / manda 5V al pin D2
 }
